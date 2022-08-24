843e57706f200899dbf87366abb8c192c2d51edb
atigfadi <fadiatig3@gmail.com>
2022-08-24 14:22:21 +0100

    first commit

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "T-shirt 1",
+    price: 30,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t1.png",
+  },
+  {
+    id: 1,
+    name: "T-shirt 2",
+    price: 25,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t2.png",
+  },
+  {
+    id: 2,
+    name: "T-shirt 3",
+    price: 20,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t3.png",
+  },
+  {
+    id: 3,
+    name: "T-shirt 4",
+    price: 26,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t4.png",
+  },
+  {
+    id: 4,
+    name: "T-shirt 5",
+    price: 30,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t5.png",
+  },
+  {
+    id: 5,
+    name: "T-shirt 6",
+    price: 40,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t6.png",
+  },
+];
